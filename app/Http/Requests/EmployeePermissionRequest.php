33266270e0ba7940420f8506fe910c8a2f7e0808
isro24 <alamat_email_anda@example.com>
2025-07-14 03:30:37 +0700

    Menambah EmployeePermissionRequest pada Requests

---
+++ app/Http/Requests/EmployeePermissionRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmployeePermissionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'type' => 'required|in:izin,sakit,cuti',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+            'reason' => 'nullable|string',
+            'attachment' => 'required|file|mimes:pdf,doc,docx|max:2048', 
+        ];
+    }
+}
